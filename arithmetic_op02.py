2ba0d8503ee9543b7c3d9bdeb0428d3c3549a9a4
yakudza2005 <aloxiddinyogmirov651@gmail.com>
2024-02-22 19:05:27 +0500

    arifmetik2

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,7 @@
 # Create a variable "a"  and assign it the value of the int 3.
-
+a=3
 # Create a variable "b"  and assign it the value of the int 4.
-
+b=4
 # Create a variable called 'answer' and assign it the sum of a and b.
+answer=a+b
+print(answer)
