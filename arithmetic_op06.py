6d745774b2e747b98b3b7eb8ee1f7273ed17785f
yakudza2005 <aloxiddinyogmirov651@gmail.com>
2024-02-23 17:43:29 +0500

    olti

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,7 +1,9 @@
 # Create a variable "a"  and assign it the value of the int 7.
-
+a=7
 # Create a variable "b"  and assign it the value of the int 3.
-
+b=3
 # Create a variable "c"  and assign it the value of the int 5.
-
+c=5
 # Create a variable called "answer" and assign it the sum of a, b and c
+ansver=a+b+c
+print(ansver)
