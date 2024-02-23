a4ccfd99cbf8cfd67d32177188ffb82d2acc9dc5
yakudza2005 <aloxiddinyogmirov651@gmail.com>
2024-02-23 17:46:41 +0500

    besh

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,7 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
+a=72
 # Create a variable "b"  and assign it the value of the int 9.
-
+b=9
 # Create a variable called "answer" and assign it the division of a by b.
+ansver=a/b
+print(ansver)
