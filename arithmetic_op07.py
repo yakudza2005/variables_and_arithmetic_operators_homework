0eb4cd24e5e9dc4015593f336318b1896d7db054
yakudza2005 <aloxiddinyogmirov651@gmail.com>
2024-02-23 17:40:30 +0500

    yetti

--- arithmetic_op07.py
+++ arithmetic_op07.py
@@ -1,7 +1,9 @@
 # Create a variable "a"  and assign it the value of the int 12.
-
+a=12
 # Create a variable "b"  and assign it the value of the int 4.
-
+b=4
 # Create a variable "c"  and assign it the value of the int 1.
-
+c=1
 # Create a variable called 'answer' and assign it the expression value: a - b + c
+ansver=a-b+c
+print(ansver)
